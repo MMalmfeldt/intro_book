b82471f4699e33af00b408a0277afaa4c062994f
Michael Malmfeldt <michael.malmfeldt@gmail.com>
2019-09-16 13:25:31 -0600

    intro book chapter one

---
+++ The_Basics/example3.rb
@@ -0,0 +1,5 @@
+movies = {:the_usual_suspects => 1998, :avengers_endgame => 2019}
+
+puts movies[:the_usual_suspects]
+puts movies[:avengers_endgame]
+
---
+++ The_Basics/example4.rb
@@ -0,0 +1,4 @@
+movies_dates = [1998, 2019]
+
+puts movies_dates[0]
+puts movies_dates[1]
