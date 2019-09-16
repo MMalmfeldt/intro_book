3d7f8bc596812cfb5925812b47117b886dea2236
Michael Malmfeldt <michael.malmfeldt@gmail.com>
2019-09-16 13:56:39 -0600

    variables chapter

---
+++ variables/example2.rb
@@ -0,0 +1,14 @@
+puts "How old are you?"
+
+age = gets.chomp.to_i
+
+puts "In ten years, you will be:"
+puts age + 10
+puts "In twenty years, you will be:"
+puts age + 20
+puts "In thirty years, you will be:"
+puts age + 30
+puts "In 40 years, you will be:"
+puts age + 40
+
+ 
---
+++ variables/example4.rb
@@ -0,0 +1,6 @@
+puts "What is your first name?"
+fname = gets.chomp
+puts "What is your last name?"
+lname = gets.chomp
+
+puts "Your whole name is #{fname} #{lname}."
